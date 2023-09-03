5c53b0fdf72c35a3436245e624bbcb4993b79de0
SalmaGamal06 <102250131+SalmaGamal06@users.noreply.github.com>
2023-09-03 15:24:52 +0300

    Create Task 4: To-Do List.h

---
+++ Task 4: To-Do List.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#ifndef CODSOFT_TO_DO_LIST_H
+#define CODSOFT_TO_DO_LIST_H
+
+class List{
+public:
+    vector<pair<string,string>> list;
+    void add(string& str);
+    void view(List& lst);
+    void remove(int x);
+    void task_completed(int x);
+
+};
+
+#endif //CODSOFT_TO_DO_LIST_H
