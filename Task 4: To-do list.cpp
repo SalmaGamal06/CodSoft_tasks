9fc0a77a26f13447f660ea3076a445562e61483d
SalmaGamal06 <102250131+SalmaGamal06@users.noreply.github.com>
2023-09-05 17:00:03 +0300

    Update Task 4: To-do list.cpp

--- Task 4: To-do list.cpp
+++ Task 4: To-do list.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <limits>
-#include "Task 4: To-Do list.h"
+#include "To-Do list.h"
 
 using namespace std;
 
-//function that takes input and checks whether its is valid or not and if it isn't it makes the user renter the input (n)
+//function that takes input and checks whether its is valid or not and if it isn't it makes the user renter the input
 
 void check_valid_input(int &n) {
     while (!(cin >> n)) {
@@ -14,11 +14,21 @@ void check_valid_input(int &n) {
     }
 }
 
+void check_valid_input(char &ch) {
+    cin >> ch;
+    while (tolower(ch) != 'y' && tolower(ch) != 'n') {
+        cout << "Please enter \'y\' or \'n\':";
+        cin >> ch;
+    }
+}
+
 //view function that prints the task list
-void List::view(List &lst) {
-    cout << "   " << "  Task    " << "     Status      " << endl;
-    for (auto i = 0; i < lst.list.size(); i++) {
-        cout << i + 1 << " -  " << lst.list[i].first << "         " << list[i].second << endl;
+void List::view() {
+
+    cout << "       " << "   Task list   " << "       " << endl;
+    cout << "===============================" << endl;
+    for (auto i = 0; i < list.size(); i++) {
+        cout << "  " << i + 1 << "-    " << list[i].first << "       " << list[i].second << endl;
     }
 }
 
@@ -41,10 +51,12 @@ void List::task_completed(int x) {
     list[x - 1].second = "Completed";
 }
 
-//run function is the core of this code
+
+//run function is the main of this code
 void run(List &lst) {
     int op, n, x;
     string str;
+    char ch;
     cout << "Welcome to to do list application!\n";
     bool exit = true;
     while (exit) {
@@ -76,38 +88,104 @@ void run(List &lst) {
                     cout << "Task " << lst.list.size() + 1 << " :";
                     cin >> str;
                     lst.add(str);
-                    cout << "Task added successfully\n";
+                    cout << "Task added successfully\n\n";
                 }
                 break;
 
                 //remove task/s from list
             case 2:
-                lst.view(lst);
-                cout << "How many tasks do you want to remove?\n";
-                check_valid_input(n);
-                while (n--) {
-                    cout << "Which task would you like to remove?\nTask";
-                    check_valid_input(x);
-                    lst.remove(x);
-                    cout << "Task removed successfully\n";
+                //check that task list is not empty
+                if (!lst.list.empty()) {
+
+                    lst.view();
+                    cout << "How many tasks do you want to remove?\n";
+                    check_valid_input(n);
+
+                    //if n is greater than the list size then a message is printed saying so and asking if the user
+                    // would like to remove all list items
+                    if (n >= lst.list.size()) {
+                        while (n >= lst.list.size()) {
+                            cout << "There are only " << lst.list.size()
+                                 << " tasks in the list would you like to remove them all ?(y/n)\n";
+                            check_valid_input(ch);
+
+                            //clear the whole task list if the user enters 'y'
+                            if (tolower(ch) == 'y') {
+                                lst.list.clear();
+                                cout << "All tasks removed successfully\n";
+                            }
+                            break;
+                        }
+                    } else {
+                        //if n is less than the list size then the program will work as usual
+                        while (n--) {
+                            cout << "Which task would you like to remove?\nTask";
+                            check_valid_input(x);
+                            lst.remove(x);
+                            cout << "Task removed successfully\n\n";
+                        }
+                    }
+
+                }
+                    //if the task list is empty then the user is asked to select a different function
+                else {
+                    cout << "The task list is empty, Please select a different function\n";
+
                 }
                 break;
 
                 //view task list
             case 3:
-                lst.view(lst);
+                if (lst.list.empty())
+                    cout << "\nTask list is empty\n";
+                else
+                    lst.view();
                 break;
 
                 //mark task as complete
             case 4:
-                lst.view(lst);
-                cout << "How many tasks have you completed?\n";
-                check_valid_input(n);
-                while (n--) {
-                    cout << "Which task have you completed?\nTask";
-                    check_valid_input(x);
-                    lst.task_completed(x);
-                    cout << "Task " << x << " completed successfully\n";
+
+                //check that task list is not empty
+                if (!lst.list.empty()) {
+                    lst.view();
+                    cout << "How many tasks have you completed?\n";
+                    check_valid_input(n);
+
+                    //if n is greater than the list size then a message is printed saying so and asking if the user
+                    // would like to remove all list items
+                    if (n >= lst.list.size()) {
+                        while (n >= lst.list.size()) {
+                            cout << "There are only " << lst.list.size()
+                                 << " tasks in the list would you like to mark them all as completed?(y/n)\n";
+                            check_valid_input(ch);
+
+                            //mark whole list as completed if the user enters 'y'
+                            if (tolower(ch) == 'y') {
+                                for (auto i = 1; i <= lst.list.size(); i++) {
+                                    lst.task_completed(i);
+                                }
+                                cout << "All tasks completed successfully\n\n";
+
+                            }
+                            break;
+                        }
+
+                    } else {
+                        //if n is less than the list size then the program will work as usual
+                        while (n--) {
+                            cout << "Which task have you completed?\nTask";
+                            check_valid_input(x);
+                            lst.task_completed(x);
+                            cout << "Task " << x << " completed successfully\n";
+                        }
+                    }
+
+                }
+                    //if the task list is empty then the user is asked to select a different function
+
+                else {
+                    cout << "The task list is empty, Please select a different function\n";
+
                 }
                 break;
 
@@ -118,7 +196,6 @@ void run(List &lst) {
 
 //main function
 int main() {
-
     List lst;
     run(lst);
 
